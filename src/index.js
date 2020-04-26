d845bf9fed646ecf919df4d85190230f71ad3f33
Anastasiya <amakhankova@shutterstock.com>
2020-04-26 18:04:03 -0400

    Add ability to delete, modify and add note

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import { onError } from 'apollo-link-error';
+import Notifications, { notify } from 'react-notify-toast';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const errorLink = onError(({ graphQLErrors }) => {
+  if(graphQLErrors) graphQLErrors.map(({ message }) => notify.show(message, 'error'))
+})
+
+const httpLink = createHttpLink({ uri: "http://localhost:8000/graphql" })
+
+const link = ApolloLink.from([errorLink, httpLink]);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
+  <Notifications />
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
